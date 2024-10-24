9a7965073fdc3c3a56e997c9aca36d34a41d4819
Christian Bonafena <141794385+ChristianBonafenaFoxi@users.noreply.github.com>
2024-10-24 13:47:51 +0700

    create static html page

--- index.js
+++
@@ -1,5 +0,0 @@
-// index.js
-const greetings = ["Hello", "Hi", "Hey", "Hola", "Bonjour"];
-const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-console.log(`${randomGreeting}, world!`);
